348f12e454dc905a2dd5258036c646ff9ceb0637
Adam Stanisławski <poncki1643@gmail.com>
2018-03-17 14:28:45 +0100

    Zapisywanie w user defaults

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,8 +12,14 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = ["Zrob cos", "kup chleb", "kup paczki"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -53,6 +59,8 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Dodaj", style: .default) { (action) in
             //akcja po kliknieciu
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             
             self.tableView.reloadData()
         }
